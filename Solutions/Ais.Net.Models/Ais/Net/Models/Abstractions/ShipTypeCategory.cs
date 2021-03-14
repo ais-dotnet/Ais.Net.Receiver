9f2e8735f95486e42fa2e55fedee6a193978c9ef
Howard van Rooijen <howard.vanrooijen@endjin.com>
2021-03-14 11:47:00 +0000

    Refactor file locations (#101)
    * Refactor file locations
    Add new ShipTypeCategory and ShipTypeGroup types to allow a "roll up" of vessel types.
    Add extension methods from converting from ShipType to ShipTypeCategory and ShipTypeGroup
    Move IStorageClient into the Ais.Net.Receiver project.
    
    * Fix missing reference

---
+++ Solutions/Ais.Net.Models/Ais/Net/Models/Abstractions/ShipTypeCategory.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="ShipTypeCategory.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Ais.Net.Models.Abstractions
+{
+    public enum ShipTypeCategory
+    {
+        NotAvailable = 0,
+        Reserved = 1,
+        WingInGround = 2,
+        SpecialCategory3 = 3,
+        HighSpeedCraft = 4,
+        SpecialCategory5 = 5,
+        Passenger = 6,
+        Cargo = 7,
+        Tanker = 8,
+        Other = 9
+    }
+}
