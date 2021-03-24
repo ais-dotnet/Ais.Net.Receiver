88f5211c37c6a3156d4d79ff2f47bdb77b1455b6
Howard van Rooijen <howard.vanrooijen@endjin.com>
2021-03-24 09:01:03 +0000

    Add option to delay to FileStreamNmeaReceiver line yielding (#104)
    * Add the ability to introduce a delay while yielding the data capture file so that you can throttle data production and not overwhelm the consumer.
    
    * Remove unused fields

--- Solutions/Ais.Net.Receiver/Ais/Net/Receiver/Receiver/FileStreamNmeaReceiver.cs
+++ Solutions/Ais.Net.Receiver/Ais/Net/Receiver/Receiver/FileStreamNmeaReceiver.cs
@@ -9,19 +9,23 @@ namespace Ais.Net.Receiver.Receiver
     using System.IO;
     using System.Runtime.CompilerServices;
     using System.Threading;
+    using System.Threading.Tasks;
 
     public class FileStreamNmeaReceiver : INmeaReceiver
     {
         private readonly string path;
+        private readonly TimeSpan delay = TimeSpan.Zero;
 
         public FileStreamNmeaReceiver(string path)
         {
             this.path = path;
         }
-
-        public int RetryAttemptLimit { get; }
-
-        public TimeSpan RetryPeriodicity { get; }
+        
+        public FileStreamNmeaReceiver(string path, TimeSpan delay)
+        {
+            this.path = path;
+            this.delay = delay;
+        }
 
         public async IAsyncEnumerable<string> GetAsync([EnumeratorCancellation] CancellationToken cancellationToken = default)
         {
@@ -32,6 +36,11 @@ namespace Ais.Net.Receiver.Receiver
                 if (cancellationToken.IsCancellationRequested)
                 {
                     break;
+                }
+
+                if (this.delay > TimeSpan.Zero)
+                {
+                    await Task.Delay(this.delay, cancellationToken).ConfigureAwait(false);
                 }
 
                 string? line = await sr.ReadLineAsync().ConfigureAwait(false);
